d9d5edc08fafe9b6a87fe6477a5e52d2171e7fe4
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2018-09-21 12:53:26 +0200

    change with React

--- script.js
+++ script.js
@@ -1,56 +1,90 @@
-class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
-    }
-
-    reset() {
-        this.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        }
-    }
-
-    print() {
-        this.display.innerText = this.format(this.times);
+class Stopwatch extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			times: {
+	            minutes: 0,
+	            seconds: 0,
+	            miliseconds: 0
+        	},
+        	score: [] 
+		};
+		this.label = 'Start'
 	}
 
 	format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
 	}
 
-	start() {
+	startStop() {
 	    if (!this.running) {
 	        this.running = true;
-	        this.watch = setInterval(() => this.step(), 10);
-	        
+	        this.label = 'Stop';
+	        this.watch = setInterval(() => this.step(), 10); 
+
+	    } else {
+	    	this.running = false;
+	    	this.label = 'Start';
+		    const score = this.state.score.slice();
+		    score.push(this.format(this.state.times));
+		    this.setState({ score });
+		    clearInterval(this.watch);
 	    }
 	}
 
 	step() {
 	    if (!this.running) return;
 	    this.calculate();
-	    this.print();
 	}
 
 	calculate() {
-	    this.times.miliseconds += 1;
-	    if (this.times.miliseconds >= 100) {
-	        this.times.seconds += 1;
-	        this.times.miliseconds = 0;
+		const times = Object.assign({}, this.state.times);
+	    times.miliseconds += 1;
+	    if (times.miliseconds >= 100) {
+	        times.seconds += 1;
+	        times.miliseconds = 0;
 	    }
-	    if (this.times.seconds >= 60) {
-	        this.times.minutes += 1;
-	        this.times.seconds = 0;
+	    if (times.seconds >= 60) {
+	        times.minutes += 1;
+	        times.seconds = 0;
 	    }
+	    this.setState({
+	    	times //w ES6 "times:times" to to samo co "times"
+	    })
 	}
 
-	stop() {
-	    this.running = false;
-	    clearInterval(this.watch);
+
+	reset() {
+        this.setState({
+            times: {
+	            minutes: 0,
+	            seconds: 0,
+	            miliseconds: 0,
+	        },
+	        score: [],
+        });
+    }
+
+	render() {
+		return (
+			<div>
+				<div className="flex-cont">
+					<div className="stopwatch animated rubberBand">{this.format(this.state.times)}</div>
+					<nav>
+						<div className="jumbotron">
+							<a href="#" className="button start" onClick={this.startStop.bind(this)} >{this.label}</a>
+							<a href="#" className="button reset" onClick={this.reset.bind(this)}>Reset</a>
+						</div>
+					</nav>  
+				</div>
+				<div className="spacer-score" id="scores">
+					<p>Scores:</p>
+					<ul>
+						{this.state.score.map((curr, i) => <li key={i}> {curr} </li>)}
+					</ul>
+				</div>
+			</div>
+		);
 	}
 }
 
@@ -62,45 +96,4 @@ function pad0(value) {
     return result;
 }
 
-let timer = document.getElementById('timer');
-let scores = document.getElementById('scores');
-let clickCounter = 0;
-
-let startStopButton = document.getElementById('start');
-startStopButton.addEventListener('click', () => {
-
-	timer.classList.toggle('rubberBand')
-
-	if (startStopButton.classList.contains('start')) {
-
-		stopwatch.start()
-		startStopButton.classList.remove('start')
-		startStopButton.innerHTML = 'Stop'
-
-	} else {
-
-		var scoresDisplay = document.createElement('div');
-		clickCounter ++;
-		scoresDisplay.innerHTML = clickCounter +'. ' + stopwatch.format(stopwatch.times)
-		scores.appendChild(scoresDisplay)
-		
-		stopwatch.stop()
-		startStopButton.classList.add('start')
-		startStopButton.innerHTML = 'Start'		
-	}
-	
-});
-
-
-let resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => {
-	clickCounter = 0;
-	stopwatch.reset()
-	stopwatch.print()
-	scores.innerHTML = '<p> Scores: </p>'
-	}
-);
-
-
-const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
+ReactDOM.render(<Stopwatch />, document.getElementById('timer'));
